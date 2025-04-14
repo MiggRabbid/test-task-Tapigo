6f332a0b1a0a870da83651938fd122bf4f2206b8
MiggRabbid <mig26@inbox.ru>
2025-04-15 00:54:59 +0300

    Add: base components

---
+++ src/features/TaskList.vue
@@ -0,0 +1,42 @@
+<!-- src/features/tasks/components/TaskList.vue -->
+<template>
+  <v-container>
+    <v-list>
+      <TaskItem v-for="task in tasks" :key="task.id" :task="task" />
+    </v-list>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { computed, onBeforeMount } from 'vue';
+import TaskItem from '@/shared/components/TaskItem.vue';
+import { fetchTasks } from '@/api/api';
+import { useActions } from '@/shared/composables/useActions';
+import { useRedux } from '@/shared/composables/useRedux';
+import { useLocalStorage } from '@/shared/composables/useLocalStorage';
+
+export default {
+  name: 'TaskList',
+  components: { TaskItem },
+  setup() {
+    const { state } = useRedux();
+    const { setTasks } = useActions();
+    const { getItem } = useLocalStorage();
+    onBeforeMount(async () => {
+      const oldTasks = getItem('tasks');
+      if (oldTasks) {
+        setTasks(oldTasks);
+        return;
+      }
+      const tasks = await fetchTasks();
+      if (tasks) {
+        setTasks(tasks);
+      }
+    });
+
+    const tasks = computed(() => state.tasks);
+
+    return { tasks };
+  },
+};
+</script>
---
+++ src/shared/components/TaskItem.vue
@@ -0,0 +1,42 @@
+<!-- src/features/tasks/components/TaskItem.vue -->
+<template>
+  <v-list-item>
+    <v-list-item-content>
+      <v-list-item-title :class="{ 'task-done': task.done }">
+        {{ task.title }}
+      </v-list-item-title>
+    </v-list-item-content>
+    <v-list-item-action>
+      <v-checkbox :input-value="task.done" @change="clickOnTasks" />
+    </v-list-item-action>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+import { useActions } from '@/shared/composables/useActions';
+
+export default {
+  name: 'TaskItem',
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const { setTaskState } = useActions();
+
+    const clickOnTasks = () => {
+      setTaskState(props.task.id);
+    };
+
+    return { clickOnTasks };
+  },
+};
+</script>
+
+<style scoped>
+.task-done {
+  text-decoration: line-through;
+}
+</style>
