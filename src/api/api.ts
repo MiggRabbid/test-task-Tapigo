21f53bd06b28ab1db268cd4735c54220f2d75cf0
MiggRabbid <mig26@inbox.ru>
2025-04-15 09:58:51 +0300

    Fix: api path

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,10 @@
 import type { TypeTaskList } from '@/types/types';
 
 export const fetchTasks = async (): Promise<TypeTaskList | void> => {
-  console.group('----- fetchTasks');
   try {
-    const response = await fetch('/data/tasks.json');
-    console.log('response -', response);
+    const response = await fetch('data/tasks.json');
     return await response.json();
   } catch (error) {
     console.error(error);
-  } finally {
-    console.groupEnd();
   }
 };
